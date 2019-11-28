8ba6b613caae8db1e5f8e82e60559c461bdc1e33
Stephen Wood <swood@netflix.com>
2019-11-28 15:41:32 -0800

    Flush every 3k articles since we have much more mem.

--- host-tools/offline-renderer/mediawiki-offline/wr_parser_sa.php
+++ host-tools/offline-renderer/mediawiki-offline/wr_parser_sa.php
@@ -68,7 +68,7 @@ while (!feof($fp)) {
     $line = fgets($fp, 8192);
 	if ($line == "***EOF***\n") {
 		echo wfParseTextAndWrapWOC($body);
-		if ($i++ % 1000 == 0 ) {	# flush every 1000 articles
+		if ($i++ % 3000 == 0 ) {	# flush every 3000 articles
 			ob_flush();		# otherwise PHP runs out of
 			flush();		# memory
 		}
